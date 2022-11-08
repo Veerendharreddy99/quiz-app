57be1436466695a31b8f6e4e6e08c5c677009bf2
unknown <virendarreddy2393@gmail.com>
2022-11-08 23:39:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,58 +1,27 @@
-import React, { Fragment,useEffect } from 'react';
-import Carousel from './component/Carousel/Carousel';
-import Footer from './component/Footer/Footer';
-import Header  from './component/Header/Header';
-import Login from './component/Auth/Login';
-import Register from './component/Auth/Register';
-import Alert from './component/Layout/Alert';
-import store from './store';
-import {Provider} from 'react-redux';
-import setAuthToken from './utils/setAuthToken';
-import Dashboard from './component/Dashboard/dashboard';
-import {loadUser} from './action/auth';
-import PrivateRoute from './component/routing/PrivateRoute';
-import IndProduct from './component/Products/IndProduct';
+import React,{useState} from 'react';
+import Questions from './questions.json';
+import Display from './Display';
+import Model from './Model'
 
-import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
-import CartProducts from './component/Products/CartProducts';
-import OrderProducts from './component/Products/OrderProducts';
-import PlaceOrder from './component/Products/PlaceOrder';
+const App = () => {
+  const [marks,setMarks] = useState([]);
+  let tempMarks = [];
+  const handleChange = (index,mark) =>{
+    tempMarks = marks
+    tempMarks[index] = mark;
+    setMarks([...tempMarks]);
+   
+  }
 
-
-const App =() =>{
-  useEffect(() => {
-    // check for token in LS
-    if (localStorage.token) {
-      setAuthToken(localStorage.token);
-    }
-    store.dispatch(loadUser());
-
-    // log user out from all tabs if they log out in one tab
-    window.addEventListener('storage', () => {
-      if (!localStorage.token) store.dispatch({ type: "LOGOUT" });
-    });
-  }, []);
   return (
-    <Provider store={store}>
-    <Router>
-      <Fragment>
-        <Header />
-        <Alert />
-            <Switch>
-                <Route exact path="/" component={Carousel}/>
-                <Route exact path="/login" component={Login}/>
-                <Route exact path="/register" component={Register}/>
-                <PrivateRoute exact path="/indproduct/:data" component={IndProduct}/>
-                <PrivateRoute exact path="/dashboard" component={Dashboard}/>
-                <PrivateRoute exact path="/cart" component={CartProducts}/>
-                <PrivateRoute exact path="/order" component={OrderProducts}/>
-                <PrivateRoute exact path="/placeorder/:data" component={PlaceOrder}/>
-            </Switch>
-        <Footer></Footer>
-      </Fragment>
-    </Router>
-    </Provider>
+    <div>
+      <center>
+        <h1>Quiz Application</h1> 
+        {Questions.map((qa,index) => <Display question={qa} handleChange={handleChange} index={index} />)}
+        <Model marks={marks}/>
+      </center>
+    </div>
   )
 }
 
-export default App;
+export default App
